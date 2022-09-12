5ee814b65216e36e170898ca832f9df133aae3b7
Mario <mario.ercegovac0@gmail.com>
2022-09-12 15:10:21 +0200

    changes

---
+++ dist/package/S3Example.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.S3Example = void 0;
+class S3Example {
+    test() {
+        console.log('test');
+    }
+}
+exports.S3Example = S3Example;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiUzNFeGFtcGxlLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vc3JjL1MzRXhhbXBsZS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFBQSxNQUFhLFNBQVM7SUFDcEIsSUFBSTtRQUNGLE9BQU8sQ0FBQyxHQUFHLENBQUMsTUFBTSxDQUFDLENBQUM7SUFDdEIsQ0FBQztDQUNGO0FBSkQsOEJBSUMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgY2xhc3MgUzNFeGFtcGxlIHtcbiAgdGVzdCgpIHtcbiAgICBjb25zb2xlLmxvZygndGVzdCcpO1xuICB9XG59XG4iXX0=
